d19a7cc707a7975d3e7d1a5bd782fc26c36a462c
ZachFaria <38866135+ZachFaria@users.noreply.github.com>
2018-09-19 11:55:21 -0400

    Added clock
    Working clock that can be changed

--- Perfect-light-and-temperature.js
+++ Perfect-light-and-temperature.js
@@ -1,6 +1,11 @@
 let temp = 0
+let min = 0
+let hours = 0
 let light = 0
+let time = ""
+let adjust = 0
 let direction = 0
+let ampm = false
 input.onButtonPressed(Button.AB, () => {
     basic.showString("LIGHT=")
     light = input.lightLevel()
@@ -22,11 +27,75 @@ input.onButtonPressed(Button.A, () => {
         basic.showString("N")
     }
 })
+input.onPinPressed(TouchPin.P0, () => {
+    if (hours < 23) {
+        hours += 1
+    } else {
+        hours = 0
+    }
+})
+input.onPinPressed(TouchPin.P1, () => {
+    if (min < 59) {
+        min += 1
+    } else {
+        min = 0
+    }
+})
+input.onPinPressed(TouchPin.P2, () => {
+    ampm = !(ampm)
+})
+input.onGesture(Gesture.Shake, () => {
+    adjust = hours
+    if (ampm) {
+        if (hours > 12) {
+            adjust = hours - 12
+        } else {
+            if (hours == 0) {
+                adjust = 12
+            }
+        }
+    }
+    time = "" + adjust
+    time = "" + time + ":"
+    time = "" + time + min / 10
+    time = "" + time + min % 10
+    if (ampm) {
+        if (hours > 11) {
+            time = "" + time + "PM"
+        } else {
+            time = "" + time + "AM"
+        }
+    }
+    basic.showString(time)
+})
+ampm = false
+time = ""
+adjust = 0
+min = 0
+hours = 0
 if (light >= 200 && light <= 255 && (temp >= 20 && temp <= 30)) {
     music.beginMelody(music.builtInMelody(Melodies.Blues), MelodyOptions.Forever)
 } else {
     music.ringTone(349)
 }
 basic.forever(() => {
-	
+    basic.pause(60000)
+    if (min < 59) {
+        min += 1
+    } else {
+        min = 0
+    }
+})
+basic.forever(() => {
+    basic.pause(60000)
+    if (min < 59) {
+        min += 1
+    } else {
+        min = 0
+        if (hours < 23) {
+            hours += 1
+        } else {
+            hours = 0
+        }
+    }
 })
