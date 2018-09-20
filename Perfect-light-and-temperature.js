7cfd38d84e52a64bf00138d0c95d771ac1ef9110
ZachFaria <38866135+ZachFaria@users.noreply.github.com>
2018-09-20 10:19:16 -0400

    Fine tuned controls
    Fine tuned controls for users ease of use

--- Perfect-light-and-temperature.js
+++ Perfect-light-and-temperature.js
@@ -1,48 +1,18 @@
 let temp = 0
-let min = 0
-let hours = 0
-let light = 0
 let time = ""
+let light = 0
+let hours = 0
+let min = 0
+let ampm = false
 let adjust = 0
 let direction = 0
-let ampm = false
-input.onButtonPressed(Button.AB, () => {
+input.onGesture(Gesture.TiltRight, () => {
     basic.showString("LIGHT=")
     light = input.lightLevel()
     basic.showNumber(light)
     basic.showString(" TEMP=")
     temp = input.temperature()
     basic.showNumber(temp)
-})
-input.onButtonPressed(Button.A, () => {
-    direction = input.compassHeading()
-    basic.showNumber(direction)
-    if (direction < 135 && direction > 45) {
-        basic.showString("E")
-    } else if (direction < 225 && direction > 135) {
-        basic.showString("S")
-    } else if (direction < 315 && direction > 225) {
-        basic.showString("W")
-    } else if (direction < 45 || direction > 315) {
-        basic.showString("N")
-    }
-})
-input.onPinPressed(TouchPin.P0, () => {
-    if (hours < 23) {
-        hours += 1
-    } else {
-        hours = 0
-    }
-})
-input.onPinPressed(TouchPin.P1, () => {
-    if (min < 59) {
-        min += 1
-    } else {
-        min = 0
-    }
-})
-input.onPinPressed(TouchPin.P2, () => {
-    ampm = !(ampm)
 })
 input.onGesture(Gesture.Shake, () => {
     adjust = hours
@@ -68,13 +38,43 @@ input.onGesture(Gesture.Shake, () => {
     }
     basic.showString(time)
 })
+input.onGesture(Gesture.TiltLeft, () => {
+    direction = input.compassHeading()
+    basic.showNumber(direction)
+    if (direction < 135 && direction > 45) {
+        basic.showString("E")
+    } else if (direction < 225 && direction > 135) {
+        basic.showString("S")
+    } else if (direction < 315 && direction > 225) {
+        basic.showString("W")
+    } else if (direction < 45 || direction > 315) {
+        basic.showString("N")
+    }
+})
+input.onButtonPressed(Button.A, () => {
+    if (hours < 23) {
+        hours += 1
+    } else {
+        hours = 0
+    }
+})
+input.onButtonPressed(Button.B, () => {
+    if (min < 59) {
+        min += 1
+    } else {
+        min = 0
+    }
+})
+input.onButtonPressed(Button.AB, () => {
+    ampm = !(ampm)
+})
 ampm = false
 time = ""
 adjust = 0
 min = 0
 hours = 0
-if (light >= 200 && light <= 255 && (temp >= 20 && temp <= 30)) {
-    music.beginMelody(music.builtInMelody(Melodies.Blues), MelodyOptions.Forever)
+if (light >= 10 && light <= 255 && (temp >= 20 && temp <= 30)) {
+    music.beginMelody(music.builtInMelody(Melodies.Blues), MelodyOptions.Once)
 } else {
     music.ringTone(349)
 }
